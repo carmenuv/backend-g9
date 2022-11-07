c89a4187a8463f7442e28296b4c9c00aab61ec6a
carmenuv <carmenurizarv@gmail.com>
2022-11-06 19:42:08 -0500

    Avance sql alchemy

--- Dia-3/app.py
+++ Dia-3/app.py
@@ -1,5 +1,15 @@
 from  flask import Flask
+from config import conexion
 
 app = Flask(__name__)
+# Formato para las cademas de conexion de las base de datos:
+# dialecto://username:password@host:port/database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost:3306/flask_sqlalchemy'
+
+# configuro mi conexión de sqlalchemy con l aaplicación de flask
+conexion.init_app(app)
+
+# indicaremos la creación de las nuevs tablas que no se encuentren registradas en la bse de datos PERO esos modelos tiene que ser utilizado en el proyecto para que se pueda crear la tabla en la bd, emitirá un error si no se logró conectar a la base de datos
+conexion.create_all(app=app)
 
 app.run(debug=True)
