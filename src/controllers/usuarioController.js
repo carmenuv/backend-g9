101332a728535fa10b4fbaf97e1d02250092336c
carmenuv <carmenurizarv@gmail.com>
2022-12-14 23:01:25 -0500

    Update2

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,11 +1,14 @@
 import { Usuario } from "../models/usuarioModel.js";
+import bcryptjs from "bcryptjs";
 
 export async function registro(req, res){
   const data = req.body;
   console.log(data);
 
   try{
-    const nuevoUsuario = await Usuario.create(data);
+    // generamos el hash de la contraseña del usuario
+    const password = bcryptjs.hashSync(data.password, 10)
+    const nuevoUsuario = await Usuario.create({...data, password});
     console.log(nuevoUsuario);
 
     res.status(201).json({
@@ -18,3 +21,24 @@ export async function registro(req, res){
     });
   }
 }
+
+export async function login(req, res){
+  const data = req.body;
+  const usuarioEncontrado = await Usuario.findOne({ email: data.email });
+
+  if (!usuarioEncontrado) {
+    return res.status(404).json({
+      message: "El usuario no existe",
+    });
+  }  
+  if (bcryptjs.compareSync(data.password, usuarioEncontrado.password)) {
+    res.json({
+      message: "Bienvenido",
+    });
+  } else {
+    res.json({
+      message: "Error al ingresar, la contraseña no es valida",
+    });
+  }
+}
+
