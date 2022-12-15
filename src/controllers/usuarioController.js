0e18ee710d94885bc52f2938d9f755a144ed4368
carmenuv <carmenurizarv@gmail.com>
2022-12-14 21:58:00 -0500

    Registro de usuarios

---
+++ src/controllers/usuarioController.js
@@ -0,0 +1,20 @@
+import { Usuario } from "../models/usuarioModel.js";
+
+export async function registro(req, res){
+  const data = req.body;
+  console.log(data);
+
+  try{
+    const nuevoUsuario = await Usuario.create(data);
+    console.log(nuevoUsuario);
+
+    res.status(201).json({
+      message: "Usuario creado exitosamente",
+    });
+  } catch (error){
+    res.status(500).json({
+      message: "Error al crear el usuario",
+      content: error.message,
+    });
+  }
+}
